c36ef6b9000105feb6486ec04e7dc6fc86c4e7e9
LaameriSayf <saif.laameri@esprit.tn>
2024-03-07 12:55:01 +0100

    sayf

---
+++ Desktop/jeux/PIDev/src/Repository/AdminRepository.php
@@ -0,0 +1,61 @@
+<?php
+
+namespace App\Repository;
+
+use App\Entity\Admin;
+use App\Entity\Blog;
+use Doctrine\Bundle\DoctrineBundle\Repository\ServiceEntityRepository;
+use Doctrine\Persistence\ManagerRegistry;
+
+/**
+ * @extends ServiceEntityRepository<Admin>
+ *
+ * @method Admin|null find($id, $lockMode = null, $lockVersion = null)
+ * @method Admin|null findOneBy(array $criteria, array $orderBy = null)
+ * @method Admin[]    findAll()
+ * @method Admin[]    findBy(array $criteria, array $orderBy = null, $limit = null, $offset = null)
+ */
+class AdminRepository extends ServiceEntityRepository
+{
+    public function __construct(ManagerRegistry $registry)
+    {
+        parent::__construct($registry, Admin::class);
+    }
+    public function findUsersWhoRatedBlog(Blog $blog): array
+    {
+        return $this->createQueryBuilder('u')
+            ->join('u.rate', 'r')
+            ->andWhere('r.blog = :blog')
+            ->setParameter('blog', $blog)
+            ->getQuery()
+            ->getResult();
+    }
+
+
+    
+
+//    /**
+//     * @return Admin[] Returns an array of Admin objects
+//     */
+//    public function findByExampleField($value): array
+//    {
+//        return $this->createQueryBuilder('a')
+//            ->andWhere('a.exampleField = :val')
+//            ->setParameter('val', $value)
+//            ->orderBy('a.id', 'ASC')
+//            ->setMaxResults(10)
+//            ->getQuery()
+//            ->getResult()
+//        ;
+//    }
+
+//    public function findOneBySomeField($value): ?Admin
+//    {
+//        return $this->createQueryBuilder('a')
+//            ->andWhere('a.exampleField = :val')
+//            ->setParameter('val', $value)
+//            ->getQuery()
+//            ->getOneOrNullResult()
+//        ;
+//    }
+}
