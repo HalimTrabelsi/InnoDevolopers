49c237d7d7dd0e2372af28b36be3ebd95b0cc1b3
Halim Trabelsi <98814699+HalimTrabelsi@users.noreply.github.com>
2025-02-05 18:12:20 +0100

    pipeline test

--- Backend/Server/server.js
+++ Backend/Server/server.js
@@ -2,7 +2,6 @@ require("dotenv").config();
 const express = require("express");
 const mongoose = require("mongoose");
 const cors = require("cors");
-
 const userRoutes = require("./routes/userRoutes");
 
 const app = express();
