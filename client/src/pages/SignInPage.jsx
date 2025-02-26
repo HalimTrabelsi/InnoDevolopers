f5114dfe69058c6063e410b3024f61b4eec5ed85
Halim Trabelsi <98814699+HalimTrabelsi@users.noreply.github.com>
2025-02-26 03:06:59 +0100

    commit

--- client/src/pages/SignInPage.jsx
+++ client/src/pages/SignInPage.jsx
@@ -1,10 +1,10 @@
 import React from "react";
 import SignInLayer from "../components/SignInLayer";
-
+import ThemeToggleButton from "../helper/ThemeToggleButton";
 const SignInPage = () => {
   return (
     <>
-
+      <ThemeToggleButton/>
       {/* SignInLayer */}
       <SignInLayer />
 
