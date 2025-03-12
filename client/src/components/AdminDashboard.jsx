5456e4b9585d4de60184518d5fbd1408ebaa6b74
Halim Trabelsi <98814699+HalimTrabelsi@users.noreply.github.com>
2025-03-12 06:57:30 +0100

    jenkinsfile

--- client/src/components/AdminDashboard.jsx
+++ client/src/components/AdminDashboard.jsx
@@ -2,18 +2,19 @@ import React from 'react'
 
 import LatestRegisteredOne from './child/LatestRegisteredOne';
 import UnitCountOne from './child/UnitCountOne';
-import TextGeneratonLayer from './CompilanceLayer';
+import ComplianceLayer from './CompilanceLayer';
 
 const AdminDashboard = () => {
 
     return (
-        <>
-                <UnitCountOne />
-                <section className="row gy-4 mt-1">
-                <LatestRegisteredOne />
-                <TextGeneratonLayer/>
+        <>  
+        <section className="row gy-4 mt-1">
 
-            </section>
+                <UnitCountOne />
+                <LatestRegisteredOne />
+                <ComplianceLayer/>
+
+         </section>
         </>
 
 
