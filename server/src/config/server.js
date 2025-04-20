0656efe01116bab09125557749207bc78a5172bd
MahmoudAbdulkareem <100515746+MahmoudAbdulkareem@users.noreply.github.com>
2025-04-20 23:45:39 +0100

    Update server.js

--- server/src/config/server.js
+++ server/src/config/server.js
@@ -3,14 +3,24 @@ const connectDB = require('./db');
 const dotenv = require('dotenv');
 const userRoutes = require('../routes/userRoute');
 const profileRoutes = require('../routes/profileRoute');
-const generateRoutes = require('../routes/generateRoute');
 const bodyParser = require('body-parser');
 const cors = require("cors");
+const session = require("express-session");
+const passport = require("passport");
+const http = require("http");
+const { Server } = require("socket.io");
+const multer = require("multer");
+const mongoose = require("mongoose");
+const authRoutes = require("../routes/authRoutes");
+const userStatsRoutes = require("../routes/userStats");
+const cashflowRoutes = require("../routes/cashflow");
+const accountsRoutes = require("../routes/accounts");
+const transactionsRoutes = require("../routes/transactions");
+const authRoute = require("../routes/refresh");
+const Transaction = require("../models/Transaction");
+const notificationRoutes = require("../routes/notificationRoutes");
+const approvalRoutes = require("../routes/approvalRoutes");
 const upload = require('../middlewares/uploadImage');
-const aiRoutes = require('../routes/aiRoutes');
-const translationRoutes = require('../routes/translationRoutes');
-
-require('dotenv').config();
 const ListRoutes = require("../routes/listRoutes");
 
 require('dotenv').config();
@@ -109,11 +119,17 @@ const translationRoutes = require('../routes/translationRoutes');
 const tranRoutes = require('../routes/tranRoute');
 const revenueRoute = require('../routes/revenueRoute'); // Import the revenue routes
 dotenv.config();
-const app = express();
-const PORT = process.env.PORT || 5001;
 const expenseRoutes = require('../routes/expenseRoutes');
 const smsRoutes = require('../routes/smsRoute'); // Adjust the path if needed
-// Connect to MongoDB
+const aiRoutes = require('../routes/aiRoutes');
+
+const cookieParser = require("cookie-parser");
+const cryptoRoutes = require('../routes/crypto');
+const compteBanciareRoutes=require('../routes/compteBancaireRoutes')
+
+require('dotenv').config();
+
+
 connectDB();
 
 // Middleware
@@ -123,22 +139,29 @@ app.use(cors({
   origin: "http://localhost:3000",
   credentials: true
 }));
+
 // Routes
 app.use('/api/users', userRoutes);
-app.use('/api/profile', profileRoutes);
 app.use('/api/export', generateRoutes);
 app.use('/api/transactions', tranRoutes); 
 app.use('/api/revenue', revenueRoute); 
 app.use('/api/expenses', expenseRoutes);
 app.use('/api/sms', smsRoutes);
 app.use('/api/ai', aiRoutes);
-
-
-
 app.use(bodyParser.urlencoded({ extended: true }));
 app.use(bodyParser.json());
+
+app.use(cookieParser());
+app.use(express.json({ limit: "5mb" })); 
+app.use(express.urlencoded({ extended: true }));
+app.use('/api/profile', profileRoutes);
+app.use('/stripe', require('../routes/stripe'));
+app.use('/crypto', cryptoRoutes); // Transaction Routes
+app.use('/compteBancaire', compteBanciareRoutes);
 
 
 app.listen(PORT, () => {
   console.log(`🚀 Server running on port ${PORT}`);
 });
+
+
