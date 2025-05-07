2ccc4355a77a16f85592fe202e0fdf778c93411d
MahmoudAbdulkareem <100515746+MahmoudAbdulkareem@users.noreply.github.com>
2025-05-07 04:54:10 +0100

    DashBoardLayerTwenty.jsx

---
+++ client/src/components/DashBoardLayerTwenty.jsx
@@ -0,0 +1,13 @@
+import React from 'react'
+import UnitCountTwenty from './child/UnitCountTwenty'
+
+const DashBoardLayerTwenty = () => {
+  return (
+    <section className="row gy-4">
+      {/* UnitCountTwenty */}
+      <UnitCountTwenty />
+    </section>
+  )
+}
+
+export default DashBoardLayerTwenty
